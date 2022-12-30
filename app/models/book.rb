4ac4410948421c2e3b516a371b57bd40d2bb0563
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2022-12-30 06:38:02 +0000

    Add_7days_books_count

--- app/models/book.rb
+++ app/models/book.rb
@@ -25,4 +25,7 @@ class Book < ApplicationRecord
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } #先週
 
+  #過去の投稿数検索、n日前の投稿数
+  scope :created_days_ago, -> (n){ where(created_at: n.days.ago.all_day) }
+
 end
